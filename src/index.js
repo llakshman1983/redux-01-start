d9ffc5f77dcc44eefcb4e68ae37dc6d4324fac45
llakshman1983 <lakshmanakumar.l@gmail.com>
2021-01-27 20:59:49 -0500

    redux devtools chrome plugin support

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import {Provider} from 'react-redux';
@@ -14,9 +14,30 @@ const rootReducer = combineReducers({
     ctr: counterReducer,
     res: resultReducer
 });
+
+
+// Middleware
+const logger = st => {
+    return next => {
+        return action => {
+            console.log('[MIDDLEWARE] Dispatching : ', action);
+            const result = next (action);
+            console.log('[MIDDLEWARE] Next State : ', st.getState());
+            console.log('[MIDDLEWARE] Result : ', result);
+            return result;
+        }
+    }
+}
+
+
+// Dynamically injected by chrome - window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
 /* Need to connect redux to react */
 // Store Creation
-const store = createStore(rootReducer);
+// Can apply multiple middleware as an argument to applyMiddleware
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 /* <Provider store={store}> </Provider> */
 //To pass the store
 ReactDOM.render(<Provider store={store}><App /> </Provider>, document.getElementById('root'));
